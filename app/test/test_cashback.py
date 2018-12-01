a23004a98c8a24959e76acb542e658d073badd85
Student01 <student01@localhost>
2018-12-01 12:19:20 +0300

    Tests cashback adove limit

--- app/test/test_cashback.py
+++ app/test/test_cashback.py
@@ -4,3 +4,7 @@ from app.cashback import cashback
 def test_cashback_under_limit():
     result = cashback(1000)
     assert  50 == result
+
+def test_cashback_above_limit():
+    result = cashback(1_000_000)
+    assert 3_000 == result
